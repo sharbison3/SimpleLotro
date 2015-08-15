4ed3eab3c55f0612c5e1aef6220b0d92aed301eb
Sam Harbison <harbison@acm.org>
2015-08-15 09:36:59 -0400

    Snapshot

--- SimpleLotro/src/simplelotro/Ingredient.java
+++ SimpleLotro/src/simplelotro/Ingredient.java
@@ -1,44 +1,33 @@
 package simplelotro;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Ingredient {
 	String name;
 	List<Ingredient> components;
-	static Map<String, Ingredient> allIngredients;
-	
-	static {
-		clear();
-	}
 	
 	public Ingredient(String name, List<Ingredient> components) {
-		super();
+		assert name.length() > 0;
+		for (Object o : components) {
+			assert o != null;
+		}
 		this.name = name;
 		this.components = components;
-		if (allIngredients.get(name) != null) {
-			throw new RuntimeException("Duplicate ingredient: " + name);
-		}
-		allIngredients.put(name, this);
 	}
 
+	/**
+	 * Two ingredients are the same if their names are the same.
+	 */
+	public boolean equals(Object o) {
+		return (o instanceof Ingredient) &&
+				((Ingredient)o).getName().equals(this.getName());
+	}
+	
 	public String getName() {
 		return name;
 	}
 
 	public List<Ingredient> getComponents() {
 		return components;
-	}
-
-	public static Map<String, Ingredient> getAllIngredients() {
-		return allIngredients;
-	}
-
-	static public Ingredient find(String name) {
-		return allIngredients.get(name);
-	}
-	public static void clear() {
-		allIngredients = new HashMap<String, Ingredient>();
 	}
 }
--- SimpleLotro/src/simplelotro/IngredientTest.java
+++ SimpleLotro/src/simplelotro/IngredientTest.java
@@ -3,7 +3,6 @@ package simplelotro;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -11,7 +10,6 @@ public class IngredientTest {
 
 	@Test
 	public void test_Constructor() {
-		Ingredient.clear();
 		Ingredient a = new Ingredient("a name", Arrays.asList());
 		Ingredient b = new Ingredient("b name", Arrays.asList());
 		Ingredient i = new Ingredient("name of it", Arrays.asList(a, b));
@@ -23,33 +21,8 @@ public class IngredientTest {
 	
 	@Test
 	public void test_empty() {
-		Ingredient.clear();
-		assertTrue(Ingredient.getAllIngredients().size() == 0);
-		assertNull(Ingredient.find("x"));
-	}
-
-	@Test
-	public void test_find() {
-		Ingredient.clear();
-		Ingredient a = new Ingredient("a name", Arrays.asList());
-		Ingredient b = new Ingredient("b name", Arrays.asList());
-		assertTrue(Ingredient.find("a name") == a);
-		assertTrue(Ingredient.find("b name") == b);
-		assertNull(Ingredient.find("A name"));
-		assertTrue(Ingredient.getAllIngredients().size() == 2);
-	}
-
-	@Test
-	public void test_duplicate() {
-		Ingredient.clear();
+		Ingredient a = new Ingredient(null, Arrays.asList());
 		@SuppressWarnings("unused")
-		Ingredient a1 = new Ingredient("a name", Arrays.asList());
-		try {
-			@SuppressWarnings("unused")
-			Ingredient a2 = new Ingredient("a name", Arrays.asList());			
-		}
-		catch (RuntimeException e) {
-			assertTrue(e.getMessage().equals("Duplicate ingredient: a name"));
-		}
+		Ingredient b = new Ingredient("b", Arrays.asList(a, null));
 	}
 }
